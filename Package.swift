0cd7d16d2ed8a312f086ce9ea2320a12fa1e229a
Chater <me@chat3r.com>
2016-02-22 19:40:26 +0200

    text

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftServerIO",
+    name: "SwiftTest",
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
     ]
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: SwiftServerIO --workers 5 --bind 0.0.0.0:$PORT
+web: SwiftTest --port=$PORT
