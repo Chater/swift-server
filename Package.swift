200d6e8d45dfd848893deeea5c7694f2a67e67f1
Chater <me@chat3r.com>
2016-02-22 19:33:15 +0200

    Another try

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftServer",
+    name: "SwiftServerIO",
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
     ]
